90538bfe623c321996f9d292d0423fd9e1ece82c
Cindereous <kenny9438@gmail.com>
2016-11-11 16:59:22 -0800

    accepting input from console

--- javaClientForPHPService.java
+++ javaClientForPHPService.java
@@ -5,7 +5,8 @@
  */
 package JavaClientforPHP;
 
-
+import java.io.*;
+import java.util.Scanner;
 import java.io.IOException;
 import okhttp3.FormBody;
 import okhttp3.MediaType;
@@ -26,8 +27,8 @@ public class javaClientForPHPService {
 
   OkHttpClient client = new OkHttpClient();
 
-  String post(String url) throws IOException {
-    RequestBody body = RequestBody.create(mediaType, "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\nme1\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\nme2\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\nme3\r\n-----011000010111000001101001--");
+  String post(String url, String username, String email, String password) throws IOException {
+    RequestBody body = RequestBody.create(mediaType, "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\n"+username+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\n"+email+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n"+password+"\r\n-----011000010111000001101001--");
     /*
     RequestBody body = RequestBody.create(JSON, json);
     */
@@ -50,7 +51,14 @@ public class javaClientForPHPService {
   public static void main(String[] args) throws IOException {
    javaClientForPHPService example = new javaClientForPHPService();
    // String json = example.register("clienttest", "clientemail","clientpassword");
-    String response = example.post("https://teaminsecurity.club/login_api/register.php");
+    Scanner sc = new Scanner(System.in);
+    System.out.print("enter username: ");
+    String uname= sc.nextLine();
+    System.out.print("enter email");
+    String umail = sc.nextLine();
+    System.out.print("enter password");
+    String upass = sc.nextLine();
+    String response = example.post("https://teaminsecurity.club/login_api/register.php",uname,umail,upass);
     //String response = example.post("https://teaminsecurity.club/login_api/register.php", json);
     System.out.println(response);
   }
