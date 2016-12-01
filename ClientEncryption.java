52a91470e25f8b0acfd3a003827a3533da6b4857
MenjiShad <jamesdinh14@gmail.com>
2016-11-30 20:00:16 -0800

    Implement AES encryption/decryption
    The Encryption class should now work properly when it encrypts and
    decrypts messages.

--- ClientEncryption.java
+++ ClientEncryption.java
@@ -7,20 +7,27 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.security.spec.KeySpec;
+import java.util.Random;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.PBEKeySpec;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
 
 /**
- * @author James
+ * @author James (for all encryption/decryption-related methods)
+ * @author Kenny (for HMAC and integrity)
  * This class will handle all the encryption and decryption of the messages
  * 
  * Simpleton class to ensure that only one instance of encryption is allowed
@@ -36,6 +43,7 @@ public class ClientEncryption {
    private static final String provider = "BC"; //Bouncy Castle
    private static final int KEY_SIZE = 256; // bits
    private static final int IV_SIZE = 16; // IV size = 16 bytes or 128 bits
+   private static final int PASSWORD_LENGTH = 16; // Used for random password
    
    // Security instance variables
    private Cipher cipher;
@@ -49,7 +57,7 @@ public class ClientEncryption {
       String cipherTransformation = ENCRYPTION_ALGORITHM + "/" + ENCRYPTION_MODE + "/" + ENCRYPTION_PADDING;
       cipher = Cipher.getInstance(cipherTransformation, provider);
       encryptionKey = generateKey();
-      integrityKey = generateKey();
+//      integrityKey = generateKey();
    }
    
    /**
@@ -72,8 +80,7 @@ public class ClientEncryption {
    
    /**
     * Re-initializes the instance
-    * Use to refresh the instance if any of the security variables need to be
-    *  changed (i.e. keys)
+    * Use to refresh the instance if any of the security variables need to be changed (i.e. keys)
     * @return a re-initialized instance of this class
     */
    public static ClientEncryption resetEncryptionInstance() {
@@ -89,7 +96,7 @@ public class ClientEncryption {
     * Encrypt the given plaintext
     * 
     * @param message - message to be encrypted
-    * @return ciphertext - the input message encrypted using the input key
+    * @return ciphertext - the input message encrypted
     * @throws InvalidAlgorithmParameterException 
     * @throws InvalidKeyException 
     * @throws BadPaddingException 
@@ -97,8 +104,7 @@ public class ClientEncryption {
     */
    public String encrypt(String plaintext) throws InvalidKeyException,
       InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-      SecureRandom secureRandom = new SecureRandom();
-      IvParameterSpec iv = generateIV(secureRandom);
+      IvParameterSpec iv = generateIV();
       
       cipher.init(Cipher.ENCRYPT_MODE, encryptionKey, iv);
       byte[] encryptedMessage = cipher.doFinal(plaintext.getBytes());
@@ -107,12 +113,12 @@ public class ClientEncryption {
       byte[] ciphertext = new byte[IV_SIZE + encryptedMessage.length];
       
       // Prepend IV to the ciphertext 
-      System.arraycopy(iv, 0, ciphertext, 0, IV_SIZE); 
+      System.arraycopy(iv.getIV(), 0, ciphertext, 0, iv.getIV().length); 
       
       // Attach the encrypted message 
       System.arraycopy(encryptedMessage, 0, ciphertext, IV_SIZE, encryptedMessage.length);
       
-      return new String(ciphertext);
+      return Base64.toBase64String(ciphertext);
    }
    
    /**
@@ -120,18 +126,30 @@ public class ClientEncryption {
     * 
     * @param ciphertext
     * @param key
-    * @param iv
-    * @return plaintext
+    * @return plaintext - the original message decrypted
     * @throws InvalidAlgorithmParameterException 
     * @throws InvalidKeyException 
     * @throws BadPaddingException 
     * @throws IllegalBlockSizeException 
     */
-   public String decrypt(String ciphertext, Key key, IvParameterSpec iv)
+   public String decrypt(String ciphertext, Key key)
          throws InvalidKeyException, InvalidAlgorithmParameterException,
          IllegalBlockSizeException, BadPaddingException {
-      cipher.init(Cipher.DECRYPT_MODE, key, iv);
-      byte[] plaintext = cipher.doFinal(ciphertext.getBytes());
+      byte[] decodedBytes = Base64.decode(ciphertext);
+      
+      // Extract the IV
+      byte[] iv = new byte[IV_SIZE];
+      System.arraycopy(decodedBytes, 0, iv, 0, IV_SIZE);
+      
+      cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+      
+      // Extract the message
+      int messageLengthInBytes = decodedBytes.length - IV_SIZE;
+      byte[] decodedMessage = new byte[messageLengthInBytes];
+      System.arraycopy(decodedBytes, IV_SIZE, decodedMessage, 0, messageLengthInBytes);
+      
+      // Decrypt
+      byte[] plaintext = cipher.doFinal(decodedMessage);
       return new String(plaintext);
    }
    
@@ -143,39 +161,59 @@ public class ClientEncryption {
     * @throws NoSuchAlgorithmException 
     */
    private Key generateKey() throws NoSuchAlgorithmException, NoSuchProviderException {
-      KeyGenerator keyGenerator = KeyGenerator.getInstance(ENCRYPTION_ALGORITHM, provider);
+      KeyGenerator keyGenerator = KeyGenerator.getInstance(ENCRYPTION_ALGORITHM);
       keyGenerator.init(KEY_SIZE);
+      
       return keyGenerator.generateKey();
    }
    
    /**
     * Generate an initialization vector for the encryption process (i.e. AES/CTR)
     * 
-    * @param random - A SecureRandom source to produce a pseudorandom IV 
     * @return IV - initialization vector
     */
-   private IvParameterSpec generateIV(SecureRandom random) {
-      // SecureRandom will automatically seed itself on the nextBytes call
+   private IvParameterSpec generateIV() {
       byte[] ivBytes = new byte[16];
+      
+      // SecureRandom will automatically seed itself on the nextBytes call
+      SecureRandom random = new SecureRandom();
       random.nextBytes(ivBytes);
       
       return new IvParameterSpec(ivBytes);
    }
+
    /**
    * HMAC SHA 256
    */
    public String HmacSHA256(String ciphertext) throws Exception{
-   Mac sha256_HMAC= Mac.getInstance("HmacSHA256");
-   mac.init(new SecretKeySpec(integrityKey, "HmacSHA256");
-   return String tag= Base64.encodeBase64String(sha256_HMAC.doFinal(ciphertext.getBytes());
+      Mac sha256_HMAC= Mac.getInstance("HmacSHA256");
+      mac.init(new SecretKeySpec(integrityKey, "HmacSHA256");
+      return String tag= Base64.encodeBase64String(sha256_HMAC.doFinal(ciphertext.getBytes());
    }
+   
    public String CipherTagConcatenate(String ciphertext, String HmacTag){
-	   return String combined = ciphertext+HmacTag;
+	   return String combined = ciphertext+HmacTag; // error here
    }
+   
    public void HmacVerify(String tag1, String tag2){
 	   if(tag1.equals(tag2)){
 		   System.out.println("Hmac test passed!");
-	   }
-	   else System.out.println("HMAC does not match!");
+	   } else
+	      System.out.println("HMAC does not match!");
+   }
+   
+   private char[] getRandomPassword() {
+      char[] randomPassword = new char[PASSWORD_LENGTH];
+      
+      Random random = new Random();
+      for (int i = 0; i < PASSWORD_LENGTH; i++) {
+         randomPassword[i] = (char)(random.nextInt('~' - '!' + 1) + '!');
+      }
+      
+      return randomPassword;
+   }
+   
+   public Key getEncryptionKey() {
+      return encryptionKey;
    }
 }
--- clientMain.java
+++ clientMain.java
@@ -13,45 +13,63 @@ import java.util.Scanner;
  * @author Kenny
  */
 public class clientMain {
-     public static void main(String[] args) throws IOException {
-   javaClientForPHPService example = new javaClientForPHPService();
-   ClientEncryption test = new ClientEncryption();
-   // String json = example.register("clienttest", "clientemail","clientpassword");
-   String url= "https://teaminsecurity.club/login_api/login.php";
-    Scanner sc = new Scanner(System.in);
-    System.out.print("enter username: ");
-    String uname= sc.nextLine();
-    System.out.print("enter email");
-    String umail = sc.nextLine();
-    System.out.print("enter password");
-    String upass = sc.nextLine();
-    String response = example.registerPost(url,uname,umail,upass);
-    System.out.println(response);
-    
-    System.out.print("login user: ");
-    String logU = sc.nextLine();
-    System.out.print("login password: ");
-    String logP = sc.nextLine();
-    response = example.loginPost(url, logU, logP);
-    
-    //String response = example.post("https://teaminsecurity.club/login_api/register.php", json);
-    System.out.println(response);
-	System.out.print("enter message: ");
-	String message = sc.nextLine;
-	System.out.print("enter receiver: ");
-	String receiver = sc.nextLine;
-	System.out.print("enter JWT(should be automated): ");
-	String JWT = sc.nextLine;
-	String encryptedMessage= test.encryt(message);
-	String hmacTag = test.HmacSHA256(encryptedMessage);
-	String metadata= test.CipherTagConcatenate(hmacTag, encryptedMessage)
-	response = example.messagePost(url,JWT,metadata,receiver);
-	System.out.println(response);
-	
-	System.out.print("Enter JWT(should be automated): ");
-	String JWT1 = sc.nextLine;
-	response = example.messageGET(url, JWT1);
-	
-  }
-    
+   public static void main(String[] args) throws IOException {
+      javaClientForPHPService example = new javaClientForPHPService();
+      ClientEncryption test = ClientEncryption.getEncryptionInstance();
+      // String json = example.register("clienttest",
+      // "clientemail","clientpassword");
+      String url = "https://teaminsecurity.club/login_api/login.php";
+      Scanner sc = new Scanner(System.in);
+      System.out.print("enter username: ");
+      String uname = sc.nextLine();
+      System.out.print("enter email");
+      String umail = sc.nextLine();
+      System.out.print("enter password");
+      String upass = sc.nextLine();
+      String response = example.registerPost(url, uname, umail, upass);
+      System.out.println(response);
+
+      System.out.print("login user: ");
+      String logU = sc.nextLine();
+      System.out.print("login password: ");
+      String logP = sc.nextLine();
+      response = example.loginPost(url, logU, logP);
+
+      // String response =
+      // example.post("https://teaminsecurity.club/login_api/register.php",
+      // json);
+      System.out.println(response);
+      System.out.print("enter message: ");
+      String message = sc.nextLine();
+      System.out.print("enter receiver: ");
+      String receiver = sc.nextLine();
+      System.out.print("enter JWT(should be automated): ");
+      String JWT = sc.nextLine();
+      String encryptedMessage = test.encrypt(message);
+      String hmacTag = test.HmacSHA256(encryptedMessage);
+      String metadata= test.CipherTagConcatenate(hmacTag, encryptedMessage);
+      response = example.messagePost(url, JWT, metadata, receiver);
+      System.out.println(response);
+
+      System.out.print("Enter JWT(should be automated): ");
+      String JWT1 = sc.nextLine();
+      response = example.messageGET(url, JWT1);
+
+      ClientEncryption ce = ClientEncryption.getEncryptionInstance();
+      String plaintext = "0", ciphertext = "0";
+      try {
+         ciphertext = ce.encrypt("Hello, world");
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      System.out.println(ciphertext);
+
+      try {
+         plaintext = ce.decrypt(ciphertext, ce.getEncryptionKey());
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      System.out.println(plaintext);
+   }
+
 }
